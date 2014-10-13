cf42e792edc0b472041109b555a38e129a345599
jared.hofer <jared.hofer@MU-C3-14.mullen.pvt.k12.co.us>
2014-10-13 13:32:16 -0600

    test

--- VendingMachine/src/VendingMachine.java
+++ VendingMachine/src/VendingMachine.java
@@ -1,5 +1,26 @@
 
 public class VendingMachine
 	{
-
+	private Cashier cashier;
+	private InventoryManager im;
+ static void main(String[] args)
+		{
+		
+		}
+	public void alertUser()
+		{
+		System.out.println("");
+		}
+	public void initProducts()
+		{
+		InventoryManager.allProducts.add(new ProductManager(new Product(name, price), quantity));
+		}
+	public void listen()
+		{
+		
+		}
+	public void turnOn()
+		{
+		
+		}
 	}
