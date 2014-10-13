8f22b987d3bc169ec61e6415483f43750d170e37
jared.hofer <jared.hofer@MU-C3-14.mullen.pvt.k12.co.us>
2014-10-13 13:32:16 -0600

    test

--- VendingMachine/src/InventoryManager.java
+++ VendingMachine/src/InventoryManager.java
@@ -1,5 +1,54 @@
-
+import java.util.ArrayList;
 public class InventoryManager
 	{
-
+	ArrayList<ProductManager> allProducts;
+	public void addProduct()
+		{
+		
+		}
+	public boolean attemptPurchase()
+		{
+		if()
+			{
+			return true;
+			}
+		else
+			{
+			return false;
+			}
+		}
+	public ProductManager find()
+		{
+		return;
+		}
+	public boolean isInStock()
+		{
+		if()
+			{
+			return true;
+			}
+		else
+			{
+			return false;
+			}
+		}
+	public boolean isValidItemCode()
+		{
+		if()
+			{
+			return true;
+			}
+		else
+			{
+			return false;
+			}
+		}
+	public ArrayList<ProductManager> listProducts()
+		{
+		return;
+		}
+	public ArrayList<Product> productsByPrice()
+		{
+		return;
+		}
 	}
