7b5c185e507aac6591b38754aa98bd1ccb1db7f2
jared.hofer <jared.hofer@MU-C3-14.mullen.pvt.k12.co.us>
2014-10-14 12:53:18 -0600

    new commit

--- VendingMachine/src/Product.java
+++ VendingMachine/src/Product.java
@@ -1,5 +1,29 @@
 
 public class Product
 	{
-
+	private String snackName;
+	private int costInCents;
+	public Product(String name, int price)
+		{
+		snackName = name;
+		costInCents = price;
+		}
+	public String getSnackName()
+		{
+		return snackName;
+		//work on other class 12
+		}
+	public int getCostInCents()
+		{
+		return costInCents;
+		}
+	public void setSnackName(String snackName)
+		{
+		this.snackName = snackName;
+		}
+	public void setCostInCents(int costInCents)
+		{
+		this.costInCents = costInCents;
+		}
+	
 	}
