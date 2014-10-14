6a0aae8a7fa75fc9988fb361fd867bf301cf32b6
jared.hofer <jared.hofer@MU-C3-14.mullen.pvt.k12.co.us>
2014-10-14 12:42:23 -0600

    test 2

--- VendingMachine/src/VendingMachine.java
+++ VendingMachine/src/VendingMachine.java
@@ -1,19 +1,30 @@
 
 public class VendingMachine
 	{
-	private Cashier cashier;
-	private InventoryManager im;
+	public Cashier cashier;
+	public InventoryManager im;
  static void main(String[] args)
 		{
 		
 		}
 	public void alertUser()
 		{
-		System.out.println("");
+		System.out.println("Hello.");
+		System.out.println("Inputs for inserting money must be in the form of the first letter of the coin captiolized followed immediately by the number of coins i.e. (Q1 for one quarter)");
+		System.out.println("Inputs for making a selection must be in the form of the letter of the selection followed by number i.e (a3)");
 		}
 	public void initProducts()
 		{
-		InventoryManager.allProducts.add(new ProductManager(new Product(name, price), quantity));
+		InventoryManager.allProducts.add(new ProductManager(new Product("Cheetos", 75), 10));
+		InventoryManager.allProducts.add(new ProductManager(new Product("Goldfish", 75), 10));
+		InventoryManager.allProducts.add(new ProductManager(new Product("Pretzels", 50), 7));
+		InventoryManager.allProducts.add(new ProductManager(new Product("Jolly_Ranchers", 60), 4));
+		InventoryManager.allProducts.add(new ProductManager(new Product("Oatmeal_Cookie", 100), 12));
+		InventoryManager.allProducts.add(new ProductManager(new Product("Snickers", 50), 8));
+		InventoryManager.allProducts.add(new ProductManager(new Product("Stride_Gum", 65), 3));
+		InventoryManager.allProducts.add(new ProductManager(new Product("Beef_Jerky", 140), 1));
+		InventoryManager.allProducts.add(new ProductManager(new Product("Chex_Mix", 70), 1));
+		InventoryManager.allProducts.add(new ProductManager(new Product("Popcorn", 35), 5));
 		}
 	public void listen()
 		{
@@ -21,6 +32,7 @@ public class VendingMachine
 		}
 	public void turnOn()
 		{
+		initProducts();
 		
 		}
 	}
